2d5a9358f6225761d7c3d7af0dc070c635fd6a31
Kuzniarski Jacek <jkuzniarski@gmail.com>
2014-09-22 22:18:54 +0200

    character card classes

---
+++ cthulhator/src/main/java/com/bustiblelemons/cthulhator/view/charactercard/CharacterCard.java
@@ -0,0 +1,88 @@
+package com.bustiblelemons.cthulhator.view.charactercard;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.bustiblelemons.cthulhator.R;
+import com.bustiblelemons.views.LoadingImage;
+
+import butterknife.InjectView;
+import butterknife.OnClick;
+
+/**
+ * Created by bhm on 21.09.14.
+ */
+public class CharacterCard extends RelativeLayout {
+
+    @InjectView(R.id.name)
+    TextView     nameView;
+    @InjectView(R.id.main_info)
+    TextView     mainInfoView;
+    @InjectView(R.id.extra_info)
+    TextView     extraInfoView;
+    @InjectView(android.R.id.icon)
+    LoadingImage loadingImage;
+    private View rootView;
+
+    public CharacterCard(Context context) {
+        super(context);
+        init(context, null);
+    }
+
+    public CharacterCard(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    public CharacterCard(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        rootView = LayoutInflater.from(context).inflate(R.layout.character_card, this, false);
+        nameView = (TextView) rootView.findViewById(R.id.name);
+        mainInfoView = (TextView) rootView.findViewById(R.id.short_info);
+        extraInfoView = (TextView) rootView.findViewById(R.id.top_characteristics);
+        loadingImage = (LoadingImage) rootView.findViewById(android.R.id.icon);
+        if (attrs != null) {
+            TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.CharacterCard);
+            array.recycle();
+        }
+        addView(rootView);
+    }
+
+    public void setCardInfo(CharacterInfo provider) {
+        if (provider == null) {
+            return;
+        }
+        if (nameView != null) {
+            nameView.setText(provider.getName());
+        }
+        if (mainInfoView != null) {
+            mainInfoView.setText(provider.getMainInfo());
+        }
+        if (extraInfoView != null) {
+            extraInfoView.setText(provider.getExtraInfo());
+        }
+        if (loadingImage != null) {
+            loadingImage.loadFrom(provider.getPortraitUrl());
+        }
+    }
+
+    @OnClick(R.id.menu)
+    public void onExpandMenu(ImageButton button) {
+        //TODO expand the menu onto the whole card. Use git library
+    }
+
+    public interface OnMenuItemSelected {
+        void onMenuItemSelected(MenuItem item);
+    }
+}
---
+++ cthulhator/src/main/java/com/bustiblelemons/cthulhator/view/charactercard/CharacterInfo.java
@@ -0,0 +1,14 @@
+package com.bustiblelemons.cthulhator.view.charactercard;
+
+/**
+ * Created by bhm on 22.09.14.
+ */
+public interface CharacterInfo {
+    String getName();
+
+    String getMainInfo();
+
+    String getExtraInfo();
+
+    String getPortraitUrl();
+}
