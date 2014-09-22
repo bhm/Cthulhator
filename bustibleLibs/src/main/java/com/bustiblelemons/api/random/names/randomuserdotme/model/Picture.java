1d2de48f75d44ba0a6a9b7aa5394d5505fb5c8f3
Kuzniarski Jacek <jkuzniarski@gmail.com>
2014-09-22 23:31:02 +0200

    picture model for randomuser.me

---
+++ bustibleLibs/src/main/java/com/bustiblelemons/api/random/names/randomuserdotme/model/Picture.java
@@ -0,0 +1,32 @@
+
+package com.bustiblelemons.api.random.names.randomuserdotme.model;
+
+public class Picture {
+    private String large;
+    private String medium;
+    private String thumbnail;
+
+    public String getLarge() {
+        return this.large;
+    }
+
+    public void setLarge(String large) {
+        this.large = large;
+    }
+
+    public String getMedium() {
+        return this.medium;
+    }
+
+    public void setMedium(String medium) {
+        this.medium = medium;
+    }
+
+    public String getThumbnail() {
+        return this.thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+}
