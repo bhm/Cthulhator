72c5beaf27fb0a7e265f5b7c640ef121f92ec2f7
Kuzniarski Jacek <jkuzniarski@gmail.com>
2014-11-02 21:37:08 +0100

    add recycler wrapper classes primer

---
+++ bustibleLibs/src/main/java/com/bustiblelemons/recycler/AbsRecyclerAdapter.java
@@ -0,0 +1,9 @@
+package com.bustiblelemons.recycler;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by hiv on 02.11.14.
+ */
+public abstract class AbsRecyclerAdapter extends RecyclerView.Adapter<AbsRecyclerHolder> {
+}
---
+++ bustibleLibs/src/main/java/com/bustiblelemons/recycler/AbsRecyclerHolder.java
@@ -0,0 +1,13 @@
+package com.bustiblelemons.recycler;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by hiv on 02.11.14.
+ */
+public class AbsRecyclerHolder extends RecyclerView.ViewHolder {
+    public AbsRecyclerHolder(View itemView) {
+        super(itemView);
+    }
+}
