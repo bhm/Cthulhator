688595c680e42d3862ad85757da3ea8bf49adadb
Kuzniarski Jacek <jkuzniarski@gmail.com>
2014-11-13 22:09:35 +0100

    CostomLocale.POLISH

---
+++ bustibleLibs/src/main/java/com/bustiblelemons/locales/CustomLocale.java
@@ -0,0 +1,10 @@
+package com.bustiblelemons.locales;
+
+import java.util.Locale;
+
+/**
+ * Created by hiv on 13.11.14.
+ */
+public class CustomLocale {
+    public static final Locale POLISH = new Locale("pl", "PL");
+}
