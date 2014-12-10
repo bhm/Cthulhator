cad6692c5ced399e3d368d085ff333e4533dd71d
Kuzniarski Jacek <jkuzniarski@gmail.com>
2014-12-10 21:07:33 +0100

    refactor out dead code

--- cthulhator/src/main/java/com/bustiblelemons/cthulhator/test/CharacterCard.java
+++
@@ -1,9 +0,0 @@
-package com.bustiblelemons.cthulhator.test;
-
-/**
- * Created by bhm on 20.07.14.
- */
-public interface CharacterCard {
-    int getLayoutId();
-//    List<CharacterProperty> getProperties();
-}
