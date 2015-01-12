e20b620ceee50a0de82c4ef7d17da5b85d4f997c
Kuzniarski Jacek <jkuzniarski@gmail.com>
2015-01-13 00:10:15 +0100

    AbsArgActivity move passed back object logic up

--- bustibleLibs/src/main/java/com/bustiblelemons/activities/AbsArgActivity.java
+++ bustibleLibs/src/main/java/com/bustiblelemons/activities/AbsArgActivity.java
@@ -26,7 +26,7 @@ public abstract class AbsArgActivity<T extends Parcelable> extends AbsActionBarA
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (savedInstanceState != null && savedInstanceState.containsKey(INSTANCE_ARGUMENT)) {
-            T arg = (T) savedInstanceState.getParcelable(INSTANCE_ARGUMENT);
+            T arg = savedInstanceState.getParcelable(INSTANCE_ARGUMENT);
             onInstanceArgumentRead(arg);
         }
     }
@@ -40,7 +40,7 @@ public abstract class AbsArgActivity<T extends Parcelable> extends AbsActionBarA
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        T arg = (T) savedInstanceState.getParcelable(INSTANCE_ARGUMENT);
+        T arg = savedInstanceState.getParcelable(INSTANCE_ARGUMENT);
         onInstanceArgumentRead(arg);
     }
 
@@ -50,5 +50,20 @@ public abstract class AbsArgActivity<T extends Parcelable> extends AbsActionBarA
         Intent data = new Intent();
         data.putExtra(INSTANCE_ARGUMENT, passBackdata);
         setResult(code, data);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (data != null) {
+            Bundle e = data.getExtras();
+            if (e != null && e.containsKey(INSTANCE_ARGUMENT)) {
+                T passedBack = e.getParcelable(INSTANCE_ARGUMENT);
+                if (passedBack != null) {
+                    onInstanceArgumentRead(passedBack);
+                }
+            }
+        }
     }
 }
